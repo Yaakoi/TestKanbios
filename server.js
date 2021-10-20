eb47fd71cac8807fce5394a81f0a9ab666722acd
Nicolas Moreau <nicolas.moreau@viacesi.fr>
2021-10-20 15:33:52 +0200

    Update employees route

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
-app.use("/api/employees", employees);
+app.use("/api", employees);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
