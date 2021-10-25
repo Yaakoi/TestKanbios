9e66f4558a207b7fcb35de457fcb78f3052dbe37
root <nicolas.moreau@viacesi.fr>
2021-10-25 18:18:44 +0200

    script : docker, first version in docker-compose

--- script/csvToMongo0.py
+++ script/csvToMongo0.py
@@ -2,11 +2,10 @@ import pandas as pd
 from pymongo import MongoClient
 import json
 
-client = MongoClient("127.0.0.1", 27017)
+client = MongoClient("mongodb://mongo:27017")
 db = client["test"]
 coll = db["employees"]
 data = pd.read_csv("./data.csv")
 payload = json.loads(data.to_json(orient='records'))
-coll.remove()
-coll.insert(payload)
+coll.insert_many(payload)
 print('done')
